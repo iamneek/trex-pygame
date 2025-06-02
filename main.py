31ee19c26cf5ef418bd1701783c64efa210ad79c
Neek <neeoub@gmail.com>
2025-06-02 18:40:07 +0545

    Pygame window init & some basic keyboard interaction setup along with pygame clock

--- main.py
+++ main.py
@@ -2,35 +2,50 @@ import sys
 import pygame
 import os
 
-os.environ['SDL_VIDEO_CENTERED'] = '1'
+os.environ['SDL_VIDEO_CENTERED'] = '1'  # hack to start the window in the center of the screen.
 
-pygame.init()
-window = pygame.display.set_mode((1200, 800))
-clock = pygame.time.Clock()
-FPS = 60
-pygame.display.set_caption('Trex Game')
 
-running = True
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.FPS = 60
+        pygame.display.set_caption('T. rex Runner')
+        pygame.display.set_icon(pygame.image.load('icon.png'))
+        self.running = True
+        self.characterX = 120
+        self.characterY = 600
 
-characterX = 120
-characterY = 600
+    def process_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.characterY -= 10
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                characterY -= 10
+                elif event.key == pygame.K_DOWN:
+                    self.characterY += 10
 
-            elif event.key == pygame.K_DOWN:
-                characterY += 10
+    def update(self):
+        pass
 
-    clock.tick(FPS)
-    window.fill((0, 0, 0))
-    pygame.draw.rect(window, (255, 255, 255), (characterX, characterY, 100, 100))
-    pygame.display.update()
+    def render(self):
+        self.window.fill((0, 0, 0))
+        pygame.draw.rect(self.window, (255, 255, 255), (self.characterX, self.characterY, 100, 100))
+        pygame.display.update()
 
+    def run(self):
+        while self.running:
+            self.process_input()
+            self.update()
+            self.render()
+            self.clock.tick(self.FPS)
+
+
+game = Game()
+game.run()
 pygame.quit()
